0ea71a7c4b0f159dcada1fd1ffdadd984a00d13e
22-BrandonGuerrero <brandon.guerrero@vallegrande.edu.pe>
2022-12-12 18:58:39 -0500

    cambio de usuario

--- resources/connect.js
+++ resources/connect.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/'));
 
 var conexion = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "brandon",
   password: "admin",
   database: "dbRestaurante",
 });
