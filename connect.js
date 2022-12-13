997113989c6250b2031905cc8f01c758a0b3103f
22-BrandonGuerrero <brandon.guerrero@vallegrande.edu.pe>
2022-12-12 21:57:46 -0500

    arreglo 3

--- connect.js
+++ connect.js
@@ -1,6 +1,8 @@
   const express = require('express');
+  
 
   const app = express();
+  
 
   app.get('/', (req, res) => {
 	//res.send('Hello World with express!')
@@ -14,6 +16,51 @@
   app.use(express.static(__dirname+'/'));
 
 
-  
 
- 
+
+  //Formulario Contacto
+
+  var mysql = require("mysql");
+  var cors = require("cors");
+  app.use(express.json());
+  app.use(cors());
+
+  var conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "dbRestaurante",
+  });
+
+  conexion.connect(function (error) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Conexión exitosa");
+    }
+  });
+
+  const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
+
+app.post("/api/pedido", (req, res) => {
+	let data = {
+    	userped: req.body.USERPED,
+    	emausped: req.body.EMAUSPED,
+    	celusped: req.body.CELUSPED,
+    	foodped: req.body.FOODPED,
+    	msgped: req.body.MSGPED
+	};
+	let sql = "INSERT INTO pedido SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
