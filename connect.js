98d9c18810630f0f8879b983bd5d7a239dbbbf76
22-BrandonGuerrero <brandon.guerrero@vallegrande.edu.pe>
2022-12-12 20:38:58 -0500

    a

--- connect.js
+++ connect.js
@@ -8,6 +8,12 @@
 });
 
   app.listen(3000);
-  console.log('Server on port 3000')
+  console.log('Server on port 3000');
 
-  app.use(express.static('public'));
+  //Recursos
+  app.use(express.static(__dirname+'/'));
+
+
+  
+
+ 
