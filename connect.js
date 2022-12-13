acf2bc6602a94cd5666e9e7a2244fabb3b893e76
22-BrandonGuerrero <brandon.guerrero@vallegrande.edu.pe>
2022-12-12 23:19:12 -0500

    a

--- connect.js
+++ connect.js
@@ -17,3 +17,54 @@ app.use(express.static(__dirname+'/'));
 
 
 
+
+//De acuerdo a lo que hemos instalado
+
+var mysql = require("mysql");
+
+var cors = require("cors");
+ 
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + '/'));
+ 
+//Verficar si esta informacion es correcta de acuerdo a tu localhost
+var conexion = mysql.createConnection({
+  host: "35.172.131.125",
+  user: "brandon",
+  password: "1234",
+  database: "dbRestaurante"
+});
+ 
+//Verificar si la conexion a base de datos fue exitosa ,de lo contrario te devolvera un error
+conexion.connect(function (error) {
+  if (error) {
+    console.log(error)
+    throw error;
+  } else {
+    console.log("Conexión exitosa");
+  }
+});
+
+
+
+ 
+//El contrato entre el servidor y el cliente para permitir la inserción de registros en la tabla
+app.post("/api/pedido", (req, res) => {
+	let data = {
+    	userped: req.body.USERPED,
+    	emausped: req.body.EMAUSPED,
+    	celusped: req.body.CELUSPED,
+    	foodped: req.body.FOODPED,
+    	msgped: req.body.MSGPED
+	};
+	let sql = "INSERT INTO pedido SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
