fa7e810906821b70db8c7f649d7ec47117e7b291
22-BrandonGuerrero <brandon.guerrero@vallegrande.edu.pe>
2022-12-12 20:25:49 -0500

    arreglo

--- connect.js
+++ connect.js
@@ -2,7 +2,10 @@
 
   const app = express();
 
-  app.get('/', (req, res) => res.send('Hello World with express!'));
+  app.get('/', (req, res) => {
+	//res.send('Hello World with express!')
+	res.sendFile("C:/Users/User/Desktop/Landing-Page/index.html")
+});
 
   app.listen(3000);
   console.log('Server on port 3000')
